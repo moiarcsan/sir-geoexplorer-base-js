6a3f944c5826637d1631f41758ee5607fb7eeb94
Luis Román <luisromangz@gmail.com>
2013-04-30 19:09:42 +0200

    Solution for the selection (included #78731) based in using the features in the query manager which is NOT the correct solution.

--- SelectFeatureAction.js
+++ SelectFeatureAction.js
@@ -86,6 +86,12 @@ gxp.plugins.SelectFeatureAction = Ext.extend(gxp.plugins.Tool, {
     /** public: property[featureManager]*/
     featureManager: "featuremanager",
 
+    /** public: property[readOnly]*/
+    readOnly : false,
+
+    exceptionTitle: "Save Failed",
+    exceptionText: "Trouble saving features",
+
     /** private: property[featureLayer]
      * Here we store the feature layer associated to the selected layer which we
      * will use to select features.
@@ -120,6 +126,49 @@ gxp.plugins.SelectFeatureAction = Ext.extend(gxp.plugins.Tool, {
         featureManager.on("layerchange", this._enableOrDisable, this);
         app.on("loginstatechange", this._enableOrDisable,this);
 
+        
+        var featureLayer = featureManager.featureLayer;      
+
+
+        this.control = new OpenLayers.Control.SelectFeature(
+                featureLayer, {  
+                    multiple: true,
+                    renderIntent:"temporary",
+                    eventListeners: {
+                        featurehighlighted: function(event) {
+                            var popup =  new gxp.FeatureEditPopup({
+                                allowDelete:true,
+                                feature: featureManager.featureStore.getByFeature(event.feature),
+                                width: 200,
+                                height: 250,
+                                readOnly: this.readOnly,
+                                collapsible: true,
+                                title: "Propiedades de "+ event.feature.fid
+                            });
+
+                            popup.on({
+                                "close": function(){
+                                    this._onPopupClosed(event.feature)
+                                },
+                                "featuremodified": function(popup, feature){
+                                    this._onFeatureModified(popup, feature);
+                                },
+                                "canceledit": function(popup, feature) {
+                                    featureStore.commitChanges();
+                                },
+                                scope: this
+                            });
+                            this.popup = popup;
+
+                            popup.show();
+                        },
+                        scope: this
+                    }
+                });
+
+        this.control.setMap(Viewer.getMapPanel().map);
+        Viewer.getMapPanel().map.addControl(this.control);
+
         var actions = gxp.plugins.SelectFeatureAction.superclass.addActions.apply(this, [{
             text: this.showButtonText ? this.buttonText : '',
             menuText: this.menuText,
@@ -138,14 +187,12 @@ gxp.plugins.SelectFeatureAction = Ext.extend(gxp.plugins.Tool, {
 
 
                 if(state) {
-                    // The tool is added.
-                    this.styleBackup = this.featureLayer.styleMap;
-                    this.featureLayer.styleMap= new OpenLayers.StyleMap({
-                        extendBase: true
-                    });
-                    this.featureLayer.redraw();
+                    this._applyStyles();
+                    this.control.initLayer(this.featureLayer);
+                    this.control.activate();
                 } else {
                    this._restoreStyles();
+                   this.control.deactivate();
                 }
 
             },
@@ -157,6 +204,192 @@ gxp.plugins.SelectFeatureAction = Ext.extend(gxp.plugins.Tool, {
         this._enableOrDisable();
 
         return actions;
+    },
+
+    _onFeatureModified : function(popup, feature) {
+        var featureManager = this._getFeatureManager();
+        var featureStore = featureManager.featureStore;
+        featureStore.on({
+            beforewrite: {
+                fn: function(store, action, rs, options) {
+                    if (this.commitMessage === true) {
+                        options.params.handle = this._commitMsg;
+                        delete this._commitMsg;
+                    }
+                },
+                single: true
+            },
+            beforesave: {
+                fn: function() {
+                    if (popup && popup.isVisible()) {
+                        popup.disable();
+                    }
+                    if (this.commitMessage === true) {
+                        if (!this._commitMsg) {
+                            var fn = arguments.callee;
+                            Ext.Msg.show({
+                                prompt: true,
+                                title: this.commitTitle,
+                                msg: this.commitText,
+                                buttons: Ext.Msg.OK,
+                                fn: function(btn, text) {
+                                    if (btn === 'ok') {
+                                        this._commitMsg = text;
+                                        featureStore.un('beforesave', fn, this);
+                                        featureStore.save();
+                                    }
+                                },
+                                scope: this,
+                                multiline: true
+                            });
+                            return false;
+                        }
+                    }
+                },
+                single: this.commitMessage !== true
+            },
+            write: {
+                fn: function() {
+                    if (popup) {
+                        if (popup.isVisible()) {
+                            popup.enable();
+                        }
+                        if (this.closeOnSave) {
+                            popup.close();
+                        }
+                    }
+                    var layer = featureManager.layerRecord;
+                    this.target.fireEvent("featureedit", featureManager, {
+                        name: layer.get("name"),
+                        source: layer.get("source")
+                    });
+                },
+                single: true
+            },
+            exception: {
+                fn: function(proxy, type, action, options, response, records) {
+                    var msg = this.exceptionText;
+                    if (type === "remote") {
+                        // response is service exception
+                        if (response.exceptionReport) {
+                            msg = gxp.util.getOGCExceptionText(response.exceptionReport);
+                        }
+                    } else {
+                        // non-200 response from server
+                        msg = "Status: " + response.status;
+                    }
+                    // fire an event on the feature manager
+                    featureManager.fireEvent("exception", featureManager, 
+                        response.exceptionReport || {}, msg, records);
+                    // only show dialog if there is no listener registered
+                    if (featureManager.hasListener("exception") === false && 
+                        featureStore.hasListener("exception") === false) {
+                            Ext.Msg.show({
+                                title: this.exceptionTitle,
+                                msg: msg,
+                                icon: Ext.MessageBox.ERROR,
+                                buttons: {ok: true}
+                            });
+                    }
+                    if (popup && popup.isVisible()) {
+                        popup.enable();
+                        popup.startEditing();
+                    }
+                },
+                single: true
+            },
+            scope: this
+        });                                
+        if(feature.state === OpenLayers.State.DELETE) {
+            /**
+             * If the feature state is delete, we need to
+             * remove it from the store (so it is collected
+             * in the store.removed list.  However, it should
+             * not be removed from the layer.  Until
+             * http://trac.geoext.org/ticket/141 is addressed
+             * we need to stop the store from removing the
+             * feature from the layer.
+             */
+            featureStore._removing = true; // TODO: remove after http://trac.geoext.org/ticket/141
+            featureStore.remove(featureStore.getRecordFromFeature(feature));
+            delete featureStore._removing; // TODO: remove after http://trac.geoext.org/ticket/141
+        }
+        featureStore.save();
+    },
+
+    _onPopupClosed: function(feature) {
+        if (this.readOnly === false) {
+            this.control.activate();
+        }
+        if(feature.layer && feature.layer.selectedFeatures.indexOf(feature) !== -1) {
+            //this.control.unselect(feature);
+        }
+    },
+
+    _applyStyles : function() {
+        this.styleBackup = this.featureLayer.styleMap;
+        this.featureLayer.styleMap= new OpenLayers.StyleMap({
+            styles:{
+                default : {
+                    defaultStyle: {
+                        cursor: "inherit",
+                        fillColor: "#ee9900",
+                        fillOpacity: 0.4,
+                        fontColor: "#000000",
+                        hoverFillColor: "white",
+                        hoverFillOpacity: 0.8,
+                        hoverPointRadius: 1,
+                        hoverPointUnit: "%",
+                        hoverStrokeColor: "red",
+                        hoverStrokeOpacity: 1,
+                        hoverStrokeWidth: 0.2,
+                        labelAlign: "cm",
+                        labelOutlineColor: "white",
+                        labelOutlineWidth: 3,
+                        pointRadius: 6,
+                        pointerEvents: "visiblePainted",
+                        strokeColor: "#ee9900",
+                        strokeDashstyle: "solid",
+                        strokeLinecap: "round",
+                        strokeOpacity: 1,
+                        strokeWidth: "2"
+                    }
+                },
+                delete : {
+                    defaultStyle: {
+                        display: "none"
+                    }
+                },
+                select : {
+                    defaultStyle: {
+                       cursor: "pointer",
+                        fillColor: "blue",
+                        fillOpacity: 0.4,
+                        fontColor: "#000000",
+                        hoverFillColor: "white",
+                        hoverFillOpacity: 0.8,
+                        hoverPointRadius: 1,
+                        hoverPointUnit: "%",
+                        hoverStrokeColor: "red",
+                        hoverStrokeOpacity: 1,
+                        hoverStrokeWidth: 0.2,
+                        labelAlign: "cm",
+                        labelOutlineColor: "white",
+                        labelOutlineWidth: 3,
+                        pointRadius: 6,
+                        pointerEvents: "visiblePainted",
+                        strokeColor: "blue",
+                        strokeDashstyle: "solid",
+                        strokeLinecap: "round",
+                        strokeOpacity: 1,
+                        strokeWidth: 2
+                    }
+                }
+            }
+          
+
+        });
+        this.featureLayer.redraw();
     },
 
     _restoreStyles : function() {
@@ -215,6 +448,11 @@ gxp.plugins.SelectFeatureAction = Ext.extend(gxp.plugins.Tool, {
 
             this.featureLayer = mgr.featureLayer;
 
+            
+            if(this.control.active){
+                this._applyStyles();  
+            }
+             this.control.initLayer(this.featureLayer);
             this.actions[0].enable();
         }else{
             // Disable the edit options
