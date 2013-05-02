89598837207d1598d658141c3071a7cbd6e7149b
Antonio J. Rodriguez <ajrodriguez@emergya.com>
2013-05-02 13:23:14 +0200

    Se comentan recientes cambios a falta de prueba más profunda.

--- widgets/EditionToolbar.js
+++ widgets/EditionToolbar.js
@@ -99,16 +99,16 @@ Viewer.widgets.EditionToolbar = Ext.extend(Ext.Toolbar, {
             actionTarget: 'editiontbar',
             tooltip: this.tooltipAddNewElement,
             toggleGroup: "editionTools"
-        }, {
-            ptype: 'gxp_adddatacolumn',
-            actionTarget: 'editiontbar',
-            tooltip: this.tooltipAddColumn,
-            toggleGroup: "editionTools"
-        }, {
-            ptype: 'gxp_deletedatacolumn',
-            actionTarget: 'editiontbar',
-            tooltip: this.tooltipDeleteColumn,
-            toggleGroup: "editionTools"
+//        }, {
+//            ptype: 'gxp_adddatacolumn',
+//            actionTarget: 'editiontbar',
+//            tooltip: this.tooltipAddColumn,
+//            toggleGroup: "editionTools"
+//        }, {
+//            ptype: 'gxp_deletedatacolumn',
+//            actionTarget: 'editiontbar',
+//            tooltip: this.tooltipDeleteColumn,
+//            toggleGroup: "editionTools"
         }, { //add new button before. This tool MUST BE THE LAST ALWAYS
             ptype: "vw_featureeditor",
             actionTarget: 'editiontbar',
